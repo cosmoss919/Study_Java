393decd81c0e0ffd11dbbe539cbe954b5c26805c
Park Na Jin <cosmoss9199@gmail.com>
2025-02-23 16:45:06 +0900

    3. 변수 문제풀이

---
+++ src/variable/ex/VarEx1.java
@@ -0,0 +1,18 @@
+package variable.ex;
+
+// 다음 코드에 반복해서 나오는 숫자 4,3을 다른 숫자로 한번에 변경 할 수 있도록 num1, num2를 사용하도록 변경해보세요
+//public static void main(String[] args) {
+//    System.out.println(4 + 3);
+//    System.out.println(4 - 3);
+//    System.out.println(4 * 3);
+//}
+
+public class VarEx1 {
+    public static void main(String[] args) {
+        int num1 = 4;
+        int num2 = 3;
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+    }
+}
---
+++ src/variable/ex/VarEx3.java
@@ -0,0 +1,11 @@
+package variable.ex;
+//long 타입의 변수를 선언하고, 그 변수를 10000000000(백억)으로 최기화한 후 출력하는 프로그램을 작성하세요.
+//boolean 타입의 변수를 선언하고, 그 변수를 true 로 초기화한 후 출력하는 프로그램을 작성하세요.
+public class VarEx3 {
+    public static void main(String[] args) {
+        long longVar = 10000000000L;
+        boolean booleanVar = true;
+        System.out.println(longVar);
+        System.out.println(booleanVar);
+    }
+}
